8cfaef9f601600f19a98ea3c29af3bf53b25f27d
Xocoatzin <xocoatzin@uob>
2013-08-05 19:32:21 +0100

    Save poses to file

--- PTAM/System.cc
+++ PTAM/System.cc
@@ -8,115 +8,158 @@
 #include "Tracker.h"
 #include "ARDriver.h"
 #include "MapViewer.h"
+#include <fstream>
+#define _POSIX_SOURCE
+#include <sys/stat.h>
+#include <unistd.h>
+#undef _POSIX_SOURCE
+#include <stdio.h>
 
 using namespace CVD;
 using namespace std;
 using namespace GVars3;
 
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://www.cplusplus.com/reference/clibrary/ctime/strftime/
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
 
-System::System()
-  : mGLWindow(mVideoSource.Size(), "PTAM")
-{
-  GUI.RegisterCommand("exit", GUICommandCallBack, this);
-  GUI.RegisterCommand("quit", GUICommandCallBack, this);
-  
-  mimFrameBW.resize(mVideoSource.Size());
-  mimFrameRGB.resize(mVideoSource.Size());
-  // First, check if the camera is calibrated.
-  // If not, we need to run the calibration widget.
-  Vector<NUMTRACKERCAMPARAMETERS> vTest;
-  
-  vTest = GV3::get<Vector<NUMTRACKERCAMPARAMETERS> >("Camera.Parameters", ATANCamera::mvDefaultParams, HIDDEN);
-  mpCamera = new ATANCamera("Camera");
-  Vector<2> v2;
-  if(v2==v2) ;
-  if(vTest == ATANCamera::mvDefaultParams)
+    for(int i = 0; i < 80; i ++)
     {
-      cout << endl;
-      cout << "! Camera.Parameters is not set, need to run the CameraCalibrator tool" << endl;
-      cout << "  and/or put the Camera.Parameters= line into the appropriate .cfg file." << endl;
-      exit(1);
+    	if(buf[i] == ':') buf[i] = '-';
     }
-  
-  mpMap = new Map;
-  mpMapMaker = new MapMaker(*mpMap, *mpCamera);
-  mpTracker = new Tracker(mVideoSource.Size(), *mpCamera, *mpMap, *mpMapMaker);
-  mpARDriver = new ARDriver(*mpCamera, mVideoSource.Size(), mGLWindow);
-  mpMapViewer = new MapViewer(*mpMap, mGLWindow);
-  
-  GUI.ParseLine("GLWindow.AddMenu Menu Menu");
-  GUI.ParseLine("Menu.ShowMenu Root");
-  GUI.ParseLine("Menu.AddMenuButton Root Reset Reset Root");
-  GUI.ParseLine("Menu.AddMenuButton Root Spacebar PokeTracker Root");
-  GUI.ParseLine("DrawAR=0");
-  GUI.ParseLine("DrawMap=0");
-  GUI.ParseLine("Menu.AddMenuToggle Root \"View Map\" DrawMap Root");
-  GUI.ParseLine("Menu.AddMenuToggle Root \"Draw AR\" DrawAR Root");
-  
-  mbDone = false;
-};
+    return buf;
+}
 
-void System::Run()
-{
-  while(!mbDone)
-    {
-      
-      // We use two versions of each video frame:
-      // One black and white (for processing by the tracker etc)
-      // and one RGB, for drawing.
+System::System() :
+		mGLWindow(mVideoSource.Size(), "PTAM") {
+	GUI.RegisterCommand("exit", GUICommandCallBack, this);
+	GUI.RegisterCommand("quit", GUICommandCallBack, this);
 
-      // Grab new video frame...
-      mVideoSource.GetAndFillFrameBWandRGB(mimFrameBW, mimFrameRGB);  
-      static bool bFirstFrame = true;
-      if(bFirstFrame)
-	{
-	  mpARDriver->Init();
-	  bFirstFrame = false;
+	mimFrameBW.resize(mVideoSource.Size());
+	mimFrameRGB.resize(mVideoSource.Size());
+	// First, check if the camera is calibrated.
+	// If not, we need to run the calibration widget.
+	Vector<NUMTRACKERCAMPARAMETERS> vTest;
+
+	vTest = GV3::get<Vector<NUMTRACKERCAMPARAMETERS> >("Camera.Parameters",
+			ATANCamera::mvDefaultParams, HIDDEN);
+	mpCamera = new ATANCamera("Camera");
+	Vector<2> v2;
+	if (v2 == v2)
+		;
+	if (vTest == ATANCamera::mvDefaultParams) {
+		cout << endl;
+		cout
+				<< "! Camera.Parameters is not set, need to run the CameraCalibrator tool"
+				<< endl;
+		cout
+				<< "  and/or put the Camera.Parameters= line into the appropriate .cfg file."
+				<< endl;
+		exit(1);
 	}
-      
-      mGLWindow.SetupViewport();
-      mGLWindow.SetupVideoOrtho();
-      mGLWindow.SetupVideoRasterPosAndZoom();
-      
-      if(!mpMap->IsGood())
-	mpARDriver->Reset();
-      
-      static gvar3<int> gvnDrawMap("DrawMap", 0, HIDDEN|SILENT);
-      static gvar3<int> gvnDrawAR("DrawAR", 0, HIDDEN|SILENT);
-      
-      bool bDrawMap = mpMap->IsGood() && *gvnDrawMap;
-      bool bDrawAR = mpMap->IsGood() && *gvnDrawAR;
-      
-      mpTracker->TrackFrame(mimFrameBW, !bDrawAR && !bDrawMap);
-      
-      if(bDrawMap)
-	mpMapViewer->DrawMap(mpTracker->GetCurrentPose());
-      else if(bDrawAR)
-	mpARDriver->Render(mimFrameRGB, mpTracker->GetCurrentPose());
 
-      //      mGLWindow.GetMousePoseUpdate();
-      string sCaption;
-      if(bDrawMap)
-	sCaption = mpMapViewer->GetMessageForUser();
-      else
-	sCaption = mpTracker->GetMessageForUser();
-      mGLWindow.DrawCaption(sCaption);
-      mGLWindow.DrawMenus();
-      mGLWindow.swap_buffers();
-      mGLWindow.HandlePendingEvents();
-    }
+	mpMap = new Map;
+	mpMapMaker = new MapMaker(*mpMap, *mpCamera);
+	mpTracker = new Tracker(mVideoSource.Size(), *mpCamera, *mpMap,
+			*mpMapMaker);
+	mpARDriver = new ARDriver(*mpCamera, mVideoSource.Size(), mGLWindow);
+	mpMapViewer = new MapViewer(*mpMap, mGLWindow);
+
+	GUI.ParseLine("GLWindow.AddMenu Menu Menu");
+	GUI.ParseLine("Menu.ShowMenu Root");
+	GUI.ParseLine("Menu.AddMenuButton Root Reset Reset Root");
+	GUI.ParseLine("Menu.AddMenuButton Root Spacebar PokeTracker Root");
+	GUI.ParseLine("DrawAR=0");
+	GUI.ParseLine("DrawMap=0");
+	GUI.ParseLine("Menu.AddMenuToggle Root \"View Map\" DrawMap Root");
+	GUI.ParseLine("Menu.AddMenuToggle Root \"Draw AR\" DrawAR Root");
+
+	mbDone = false;
+}
+;
+
+void System::Run() {
+	int i = 0;
+	ofstream
+		poses;
+	string
+		date = currentDateTime();
+	time_t t = time(0);
+
+	mkdir("out", S_IRWXU|S_IRGRP|S_IXGRP);
+	mkdir(("out/"+date).c_str(), S_IRWXU|S_IRGRP|S_IXGRP);
+	poses.open(("out/"+date+"/"+date + ".campose").c_str());
+
+	while (!mbDone) {
+
+		// We use two versions of each video frame:
+		// One black and white (for processing by the tracker etc)
+		// and one RGB, for drawing.
+
+		// Grab new video frame...
+		mVideoSource.GetAndFillFrameBWandRGB(mimFrameBW, mimFrameRGB);
+		static bool bFirstFrame = true;
+		if (bFirstFrame) {
+			mpARDriver->Init();
+			bFirstFrame = false;
+		}
+
+		mGLWindow.SetupViewport();
+		mGLWindow.SetupVideoOrtho();
+		mGLWindow.SetupVideoRasterPosAndZoom();
+
+		if (!mpMap->IsGood())
+			mpARDriver->Reset();
+
+		static gvar3<int> gvnDrawMap("DrawMap", 0, HIDDEN | SILENT);
+		static gvar3<int> gvnDrawAR("DrawAR", 0, HIDDEN | SILENT);
+
+		bool bDrawMap = mpMap->IsGood() && *gvnDrawMap;
+		bool bDrawAR = mpMap->IsGood() && *gvnDrawAR;
+
+		mpTracker->TrackFrame(mimFrameBW, !bDrawAR && !bDrawMap);
+
+		SE3<> cPose = mpTracker->GetCurrentPose();
+
+		if (bDrawMap)
+			mpMapViewer->DrawMap(cPose);
+		else if (bDrawAR)
+			mpARDriver->Render(mimFrameRGB, cPose);
+
+		if(mpMap->IsGood())
+		{
+			Vector<3>& translation = cPose.get_translation();
+			SO3<>& rotation = cPose.get_rotation();
+			Matrix <3,3> rotMtx = rotation.get_matrix();
+			cout << i++ << " T:"
+				<< translation
+				<< endl
+				<< rotMtx
+				<< endl;
+		}
+
+		//      mGLWindow.GetMousePoseUpdate();
+		string sCaption;
+		if (bDrawMap)
+			sCaption = mpMapViewer->GetMessageForUser();
+		else
+			sCaption = mpTracker->GetMessageForUser();
+		mGLWindow.DrawCaption(sCaption);
+		mGLWindow.DrawMenus();
+		mGLWindow.swap_buffers();
+		mGLWindow.HandlePendingEvents();
+	}
+
+	poses.close();
 }
 
-void System::GUICommandCallBack(void *ptr, string sCommand, string sParams)
-{
-  if(sCommand=="quit" || sCommand == "exit")
-    static_cast<System*>(ptr)->mbDone = true;
+void System::GUICommandCallBack(void *ptr, string sCommand, string sParams) {
+	if (sCommand == "quit" || sCommand == "exit")
+		static_cast<System*>(ptr)->mbDone = true;
 }
-
-
-
-
-
-
-
 
